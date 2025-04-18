4a19605aa82fea79387db8ae9d60f7e82c08c74e
Luke Heisler <12lukeheisler@gmail.com>
2025-04-18 08:28:12 +0000

    2nd to last commit

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
-let sword = document.querySelector('.slash-cost');
+let sword = document.querySelector(".slash-cost");
+let parsedSword = parseFloat(sword.innerHTML);
+
+let clickerCost = document.querySelector(".clicker-cost");
+let parsedClickerCost = parseFloat(clickerCost.innerHTML);
+let traineeLevel = document.querySelector(".trainee-level");
+let traineeIncrease = document.querySelector(".trainee-increase");
+let parsedClickerIncrease = parseFloat(traineeIncrease.innerHTML);
+
+let autoSwordCost = document.querySelector(".autoSword-cost");
+let parsedautoSwordCost = parseFloat(autoSwordCost.innerHTML);
+let autoSwordLevel = document.querySelector(".autoSword-level");
+let autoSwordIncrease = document.querySelector(".autoSword-increase");
+let parsedautoSwordIncrease = parseFloat(autoSwordIncrease.innerHTML);
+
+let spc = 1;
+let sps = 0;
 
 function incrementSword() {
-  sword.innerHTML = parseFloat(sword.innerHTML) + 1;
+  sword.innerHTML = Math.round(parsedSword += spc);
 }
+
+function buyClicker() {
+  if (parsedSword >= parsedClickerCost) {
+    sword.innerHTML = Math.round(parsedSword -= parsedClickerCost);
+
+    traineeLevel.innerHTML ++;
+    parsedClickerIncrease = parseFloat((parsedClickerIncrease + 1).toFixed(2));
+    traineeIncrease.innerHTML = parsedClickerIncrease;
+    spc = parsedClickerIncrease;
+
+    parsedClickerCost *= 1.25;
+    clickerCost.innerHTML = Math.round(parsedClickerCost);
+  }
+}
+
+function buyautoSword() {
+  if (parsedSword >= parsedautoSwordCost) {
+    sword.innerHTML = Math.round(parsedSword -= parsedautoSwordCost);
+
+    autoSwordLevel.innerHTML ++;
+    parsedautoSwordIncrease = parseFloat((parsedautoSwordIncrease + 2).toFixed(2));
+    autoSwordIncrease.innerHTML = parsedautoSwordIncrease;
+    sps = parsedautoSwordIncrease;
+
+    parsedautoSwordCost *= 1.25;
+    autoSwordCost.innerHTML = Math.round(parsedautoSwordCost);
+  }
+}
+
+setInterval(() => {
+  parsedSword += sps;
+  sword.innerHTML = Math.round(parsedSword);
+}, 1000)
