2bccd1e9886d76c91a5824a2c7bd200a59467e09
Luke Heisler <12lukeheisler@gmail.com>
2025-04-19 09:11:12 +0000

    Version 1

--- script.js
+++ script.js
@@ -13,6 +13,18 @@ let autoSwordLevel = document.querySelector(".autoSword-level");
 let autoSwordIncrease = document.querySelector(".autoSword-increase");
 let parsedautoSwordIncrease = parseFloat(autoSwordIncrease.innerHTML);
 
+let swordMinerCost = document.querySelector(".swordMiner-cost");
+let parsedswordMinerCost = parseFloat(swordMinerCost.innerHTML);
+let swordMinerLevel = document.querySelector(".swordMiner-level");
+let swordMinerIncrease = document.querySelector(".swordMiner-increase");
+let parsedswordMinerIncrease = parseFloat(swordMinerIncrease.innerHTML);
+
+let katanaCost = document.querySelector(".katana-cost");
+let parsedkatanaCost = parseFloat(katanaCost.innerHTML);
+let katanaLevel = document.querySelector(".katana-level");
+let katanaIncrease = document.querySelector(".katana-increase");
+let parsedkatanaIncrease = parseFloat(katanaIncrease.innerHTML);
+
 let spc = 1;
 let sps = 0;
 
@@ -45,6 +57,34 @@ function buyautoSword() {
 
     parsedautoSwordCost *= 1.25;
     autoSwordCost.innerHTML = Math.round(parsedautoSwordCost);
+  }
+}
+
+function buyswordMiner() {
+  if (parsedSword >= parsedswordMinerCost) {
+    sword.innerHTML = Math.round(parsedSword -= parsedswordMinerCost);
+
+    swordMinerLevel.innerHTML ++;
+    parsedswordMinerIncrease = parseFloat((parsedswordMinerIncrease + 14).toFixed(2));
+    swordMinerIncrease.innerHTML = parsedswordMinerIncrease;
+    sps = parsedswordMinerIncrease;
+
+    parsedswordMinerCost *= 1.35;
+    swordMinerCost.innerHTML = Math.round(parsedswordMinerCost);
+  }
+}
+
+function buykatana() {
+  if (parsedSword >= parsedkatanaCost) {
+    sword.innerHTML = Math.round(parsedSword -= parsedkatanaCost);
+
+    katanaLevel.innerHTML ++;
+    parsedkatanaIncrease = parseFloat((parsedkatanaIncrease + 60).toFixed(2));
+    katanaIncrease.innerHTML = parsedkatanaIncrease;
+    sps = parsedkatanaIncrease;
+
+    parsedkatanaCost *= 1.5;
+    katanaCost.innerHTML = Math.round(parsedkatanaCost);
   }
 }
 
